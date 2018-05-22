7501e897db3ace73e8c299fcc39b63a1d8a35c7b
Thomas Hine <guitaristtom@users.noreply.github.com>
2018-05-22 01:01:08 -0700

    Fixed some comments, added to the readme

--- README.md
+++ README.md
@@ -3,6 +3,13 @@ SSPokemonHUD
 
 SSPokemonHUD is an idea that [ShockSlayer](https://www.youtube.com/c/shockslayer "SS's YouTube Channel") had come up with and was starting to work on for his [Twitch streams](https://twitch.tv/shockslayer "SS's Twitch").
 
+You will need to host this on web server of some sort.
+
+* On Windows I'd recommend [WAMP](http://www.wampserver.com/en/) (mainly because I couldn't ever get XAMPP to work...).
+* Under Linux I'd suggest Apache.
+
+Make a copy of `team.example.json` and name it `team.json` to get started.
+
 Resources you will need to download:
 * [Assets Pack](http://www.mediafire.com/download/1m8bm8mj8z1ozrd/SSPokemonHUD-assets.zip) ([backup link](https://mega.nz/#!MddziApS!lGOZm1ZIleFFByirBnQtEqmt_86lbQvBG1kAjLWdxJQ)) - put in the /assets folder
 * [Pokemon GB Fonts](http://www.fontspace.com/jackster-productions/pokemon-gb) - put in the /fonts folder
--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
+var teamString = "";
 var teamArray = [];
-var teamJSON = "";
+/**
+ * Grabs the team data from the JSON file, and then runs the populate function
+ *
+ * @return {boolean}
+ */
 function grabTeam(){
     var teamFile = "team.json";
     $.getJSON(teamFile, {
     }).done(function(data) {
-        if (teamJSON !== JSON.stringify(data)) {
-            teamJSON = JSON.stringify(data);
+        if (teamString !== JSON.stringify(data)) {
+            teamString = JSON.stringify(data);
             teamArray = data;
             populateTeam(data);
         }
@@ -14,31 +19,52 @@ function grabTeam(){
     return true;
 };
 
+/**
+ * Populates the team with the desired data
+ *
+ * @param  {array/object}   teamData    Imported JSON team data
+ *
+ * @return {boolean}
+ */
 function populateTeam(teamData){
     var counter = 1;
     $.each(teamData, function(index, val) {
-        console.log(index+": "+val.dexnumber);
-        $(".team #" + index + " .bg").attr("src", "assets/bg_colors/"+val.bg+".jpg"); //Sets the Pokeball Background behind the info
+        // Sets the background WIP
+        $(".team #" + index + " .bg").attr("src", "assets/bg_colors/" + val.bg + ".jpg");
 
+        // Pads on a 0 or two if needed, so the file look ups are correct
         var dexNumber = pad(val.dexnumber, 3);
         if (val.shiny == 0) {
-            $(".team #" + index + " .sprite").attr("src", "assets/sprites/"+dexNumber+".gif"); //If not shiny, set as so
+            $(".team #" + index + " .sprite").attr("src", "assets/sprites/" + dexNumber + ".gif");
         } else {
-            $(".team #" + index + " .sprite").attr("src", "assets/sprites_s/"+dexNumber+".gif"); //If shiny, set as so
+            $(".team #" + index + " .sprite").attr("src", "assets/sprites_s/" + dexNumber + ".gif");
         }
 
-        $(".team #" + index + " .ball").attr("src", "assets/balls/"+val.ball+".png"); //Sets which Pokeball you caught the Pokemon in
-        $(".team #" + index + " .nickname").html(val.nickname); //Sets the nickname of the Pokemon
+        // Captured Pokeball
+        $(".team #" + index + " .ball").attr("src", "assets/balls/" + val.ball + ".png");
+
+        // Nickname
+        $(".team #" + index + " .nickname").text(val.nickname);
+
+        // Level
         if (val.level == 0) {
-            $(".team #" + index + " .level").html(""); //If set to 0, don't show Level: XXX
+            $(".team #" + index + " .level").text("");
         } else {
-            $(".team #" + index + " .level").html("Lv. " + val.level); //If set to other then 0, show Level: XXX
+            $(".team #" + index + " .level").text("Lv. " + val.level);
         }
     });
 
     return true;
 }
 
+/**
+ * Adds X amount of zeros to a number as lead padding
+ *
+ * @param  {string} num     Input number
+ * @param  {int}    size    Desired length of the number string
+ *
+ * @return {[type]}      [description]
+ */
 function pad(num, size) {
     var s = num+"";
     while (s.length < size) s = "0" + s;
