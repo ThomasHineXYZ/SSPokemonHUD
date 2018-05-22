39f9589b56c2041c2ea3aa8e72aecc80b4d7331c
Thomas Hine <guitaristtom@users.noreply.github.com>
2018-05-22 00:22:53 -0700

    Got team members all showing up and updating every half second

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 var teamArray = [];
+var teamJSON = "";
 function grabTeam(){
     var teamFile = "team.json";
     $.getJSON(teamFile, {
     }).done(function(data) {
-        teamArray = data;
-        console.log(teamArray);
-        populateTeam(data);
+        if (teamJSON !== JSON.stringify(data)) {
+            teamJSON = JSON.stringify(data);
+            teamArray = data;
+            populateTeam(data);
+        }
     });
 
     return true;
@@ -13,22 +16,25 @@ function grabTeam(){
 
 function populateTeam(teamData){
     var counter = 1;
-    $(".team #slot1 .bg").attr("src", "assets/bg_colors/"+teamData.slot1.bg+".jpg"); //Sets the Pokeball Background behind the info
+    $.each(teamData, function(index, val) {
+        console.log(index+": "+val.dexnumber);
+        $(".team #" + index + " .bg").attr("src", "assets/bg_colors/"+val.bg+".jpg"); //Sets the Pokeball Background behind the info
 
-    var dexNumber = pad(teamData.slot1.dexnumber, 3);
-    if (teamData.slot1.shiny == 0) {
-        $(".team #slot1 .sprite").attr("src", "assets/sprites/"+dexNumber+".gif"); //If not shiny, set as so
-    } else {
-        $(".team #slot1 .sprite").attr("src", "assets/sprites_s/"+dexNumber+".gif"); //If shiny, set as so
-    }
+        var dexNumber = pad(val.dexnumber, 3);
+        if (val.shiny == 0) {
+            $(".team #" + index + " .sprite").attr("src", "assets/sprites/"+dexNumber+".gif"); //If not shiny, set as so
+        } else {
+            $(".team #" + index + " .sprite").attr("src", "assets/sprites_s/"+dexNumber+".gif"); //If shiny, set as so
+        }
 
-    $(".team #slot1 .ball").attr("src", "assets/balls/"+teamData.slot1.ball+".png"); //Sets which Pokeball you caught the Pokemon in
-    $(".team #slot1 .nickname").html(teamData.slot1.nickname); //Sets the nickname of the Pokemon
-    if (teamData.slot1.level == 0) {
-        $(".team #slot1 .level").html(""); //If set to 0, don't show Level: XXX
-    } else {
-        $(".team #slot1 .level").html("Lv. " + teamData.slot1.level); //If set to other then 0, show Level: XXX
-    }
+        $(".team #" + index + " .ball").attr("src", "assets/balls/"+val.ball+".png"); //Sets which Pokeball you caught the Pokemon in
+        $(".team #" + index + " .nickname").html(val.nickname); //Sets the nickname of the Pokemon
+        if (val.level == 0) {
+            $(".team #" + index + " .level").html(""); //If set to 0, don't show Level: XXX
+        } else {
+            $(".team #" + index + " .level").html("Lv. " + val.level); //If set to other then 0, show Level: XXX
+        }
+    });
 
     return true;
 }
@@ -41,4 +47,5 @@ function pad(num, size) {
 
 $(document).ready(function() {
     grabTeam();
+    setInterval(grabTeam, 500);
 });
