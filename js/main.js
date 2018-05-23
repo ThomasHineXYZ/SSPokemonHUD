771c66662d3f61609bfe8ccc7cd329219c057c96
Thomas Hine <guitaristtom@users.noreply.github.com>
2018-05-22 21:45:44 -0700

    Added in support for eggs and an empty slot

--- js/main.js
+++ js/main.js
@@ -59,38 +59,54 @@ function grabTeam(){
 function populateTeam(teamData){
     var counter = 1;
     $.each(teamData, function(index, val) {
-        // Sets the background by grabbing the "colour" of the Pokemon, then setting that
-        var colour = colours[pkmnSpecies[val.dexnumber].color_id].identifier;
-        $("#" + index).css("background-image", "url(assets/bg_colors/" + colour + ".jpg)");
-        console.log(colour);
+        if (val.dexnumber >= 1) {
+            // Sets the background by grabbing the "colour" of the Pokemon, then setting that
+            var colour = colours[pkmnSpecies[val.dexnumber].color_id].identifier;
+            $("#" + index).css("background-image", "url(assets/bg_colors/" + colour + ".png)");
+            console.log(colour);
 
-        // Pads on a 0 or two if needed, so the file look ups are correct
-        var dexNumber = pad(val.dexnumber, 3);
-        if (val.shiny == 0) {
-            $(".team #" + index + " .sprite").attr("src", "assets/sprites/" + dexNumber + ".gif");
-        } else {
-            $(".team #" + index + " .sprite").attr("src", "assets/sprites_s/" + dexNumber + ".gif");
+            // Pads on a 0 or two if needed, so the file look ups are correct
+            var dexNumber = pad(val.dexnumber, 3);
+            if (val.shiny == 0) {
+                $("#" + index + " .sprite").attr("src", "assets/sprites/" + dexNumber + ".gif");
+            } else {
+                $("#" + index + " .sprite").attr("src", "assets/sprites_s/" + dexNumber + ".gif");
+            }
+
+            // Captured Pokeball
+            $("#" + index + " .ball").attr("src", "assets/balls/" + val.ball + ".png");
+
+            // Nickname
+            if (val.nickname != "") {
+                $("#" + index + " .nickname").text(val.nickname);
+            } else {
+                $("#" + index + " .nickname").text(basePkmn[val.dexnumber].identifier);
+            }
+
+            // Level
+            $("#" + index + " .level").text("Lv. " + val.level);
+
+        } else if (val.dexnumber == -1) {
+            // If it's an egg
+            $("#" + index).css("background-image", "url(assets/bg_colors/white.png)");
+            $("#" + index + " .sprite").attr("src", "assets/sprites/egg.gif");
+            $("#" + index + " .ball").attr("src", "assets/balls/poke.png");
+            $("#" + index + " .nickname").text("");
+            $("#" + index + " .level").text("");
+
+        } else if (val.dexnumber == 0) {
+            // If it's empty
+            $("#" + index).css("background-image", "url(assets/bg_colors/none.png)");
+
+            // Set's the sprite and ball to a 1px x 1px pixel transparent GIF
+            $("#" + index + " .sprite").attr("src", "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==");
+            $("#" + index + " .ball").attr("src", "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==");
+
+            // And empty the text areas
+            $("#" + index + " .nickname").text("");
+            $("#" + index + " .level").text("");
+
         }
-
-        // Captured Pokeball
-        $(".team #" + index + " .ball").attr("src", "assets/balls/" + val.ball + ".png");
-
-        // Nickname
-        if (val.nickname != "") {
-            $(".team #" + index + " .nickname").text(val.nickname);
-        } else {
-            $(".team #" + index + " .nickname").text(basePkmn[val.dexnumber].identifier);
-        }
-
-        // Level
-        if (val.level == 0) {
-            $(".team #" + index + " .level").text("");
-        } else {
-            $(".team #" + index + " .level").text("Lv. " + val.level);
-        }
-
-        console.log(val.dexnumber + ": " + basePkmn[val.dexnumber].identifier);
-        console.log(basePkmn);
     });
 
     return true;
@@ -142,7 +158,6 @@ $(document).ready(function() {
 
         types = csvToObject(typesCSV[0]).data;
         types.unshift({});
-
         // And finally, grab the team data, and set up a loop
         grabTeam();
         setInterval(grabTeam, 500);
