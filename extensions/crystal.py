8224f8a61bf8e57a985eb24a4e20228fe3bed2b4
Thomas <guitaristtom@users.noreply.github.com>
2018-06-03 10:44:59 -0700

    Put crystal extension in to a while loop to constantly run

--- extensions/crystal.py
+++ extensions/crystal.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from intelhex import IntelHex
 import json
+import os
+import time
 
 # Translation dictionary from hex to standard characters
 # https://github.com/pret/pokecrystal/blob/725148443f0ed0f70af747259ef49643359e92a2/charmap.asm
@@ -45,11 +47,6 @@ charmap = {
 
 }
 
-# Set up and load up the save state data
-ih = IntelHex()
-ih.fromfile('testdata/Crystal_Clear.sn1', format='bin')
-#ih.dump() # Dumps everything
-
 # Converts a variable to Hex again, but removes the 0x at the beginning,
 # and adds a leading zero if needed
 def bHex(input):
@@ -64,6 +61,8 @@ def grabName(inputs):
     for input in inputs:
         hexInput = bHex(input)
         if hexInput == "50":
+            break
+        elif hexInput == "00":
             break
 
         else:
@@ -102,139 +101,155 @@ def checkShiny(input):
             return True
     return False
 
-# Grabs and stores all of the Pokemon's data
-# -BB48 offset change from the Crystal RAM map
-# https://datacrystal.romhacking.net/wiki/Pokémon_Crystal:RAM_map
-pokemon = {
-    "slot1": {
-        "dexnumber": checkDex(ih[0x00002190]),
-        "nickname": grabName([\
-            ih[0x000022F9], \
-            ih[0x000022FA], \
-            ih[0x000022FB], \
-            ih[0x000022FC], \
-            ih[0x000022FD], \
-            ih[0x000022FE], \
-            ih[0x000022FF], \
-            ih[0x00002300], \
-            ih[0x00002301], \
-            ih[0x00002302]\
-        ]),
-        "ball": "",
-        "shiny": checkShiny([
-            bHex(ih[0x000021AC]),
-            bHex(ih[0x000021AD])
-        ]),
-        "level": ih[0x000021B6]
-    },
-    "slot2": {
-        "dexnumber": checkDex(ih[0x00002191]),
-        "nickname": grabName([\
-            ih[0x00002304], \
-            ih[0x00002305], \
-            ih[0x00002306], \
-            ih[0x00002307], \
-            ih[0x00002308], \
-            ih[0x00002309], \
-            ih[0x0000230A], \
-            ih[0x0000230B], \
-            ih[0x0000230C], \
-            ih[0x0000230D]\
-        ]),
-        "ball": "",
-        "shiny": checkShiny([
-            bHex(ih[0x000021DC]),
-            bHex(ih[0x000021DD])
-        ]),
-        "level": ih[0x000021E6]
-    },
-    "slot3": {
-        "dexnumber": checkDex(ih[0x00002192]),
-        "nickname": grabName([\
-            ih[0x0000230F], \
-            ih[0x00002310], \
-            ih[0x00002311], \
-            ih[0x00002312], \
-            ih[0x00002313], \
-            ih[0x00002314], \
-            ih[0x00002315], \
-            ih[0x00002316], \
-            ih[0x00002317], \
-            ih[0x00002318]\
-        ]),
-        "ball": "",
-        "shiny": checkShiny([
-            bHex(ih[0x0000220C]),
-            bHex(ih[0x0000220D])
-        ]),
-        "level": ih[0x00002216]
-    },
-    "slot4": {
-        "dexnumber": checkDex(ih[0x00002193]),
-        "nickname": grabName([\
-            ih[0x0000231A], \
-            ih[0x0000231B], \
-            ih[0x0000231C], \
-            ih[0x0000231D], \
-            ih[0x0000231E], \
-            ih[0x0000231F], \
-            ih[0x00002320], \
-            ih[0x00002321], \
-            ih[0x00002322], \
-            ih[0x00002323]\
-        ]),
-        "ball": "",
-        "shiny": checkShiny([
-            bHex(ih[0x0000223C]),
-            bHex(ih[0x0000223D])
-        ]),
-        "level": ih[0x00002246]
-    },
-    "slot5": {
-        "dexnumber": checkDex(ih[0x00002194]),
-        "nickname": grabName([\
-            ih[0x00002325], \
-            ih[0x00002326], \
-            ih[0x00002327], \
-            ih[0x00002328], \
-            ih[0x00002329], \
-            ih[0x0000232A], \
-            ih[0x0000232B], \
-            ih[0x0000232C], \
-            ih[0x0000232D], \
-            ih[0x0000232E]\
-        ]),
-        "ball": "",
-        "shiny": checkShiny([
-            bHex(ih[0x0000226C]),
-            bHex(ih[0x0000226D])
-        ]),
-        "level": ih[0x00002276]
-    },
-    "slot6": {
-        "dexnumber": checkDex(ih[0x00002195]),
-        "nickname": grabName([\
-            ih[0x00002330], \
-            ih[0x00002331], \
-            ih[0x00002332], \
-            ih[0x00002333], \
-            ih[0x00002334], \
-            ih[0x00002335], \
-            ih[0x00002336], \
-            ih[0x00002337], \
-            ih[0x00002338], \
-            ih[0x00002339]\
-        ]),
-        "ball": "",
-        "shiny": checkShiny([
-            bHex(ih[0x0000229C]),
-            bHex(ih[0x0000229D])
-        ]),
-        "level": ih[0x000022A6]
-    }
-}
+# Loop to check for differences in the save state file
+fileName = 'testdata/Crystal_Clear.sn1'
+lastModifiedTime = os.path.getmtime(fileName)
+while(True):
+    if(os.path.getmtime(fileName) > lastModifiedTime):
+        # Resets the last modified time code
+        lastModifiedTime = os.path.getmtime(fileName)
 
-# Finally, writes it to the team.json file
-file = open("../team.json","w")
-file.write(json.dumps(pokemon))
-file.close()
+        # Outputs a message for the user
+        print("Save State was made on %s" % lastModifiedTime)
+
+        # Set up and load up the save state data
+        ih = IntelHex()
+        ih.fromfile(fileName, format='bin')
+
+        # Grabs and stores all of the Pokemon's data
+        # -BB48 offset change from the Crystal RAM map
+        # https://datacrystal.romhacking.net/wiki/Pokémon_Crystal:RAM_map
+        pokemon = {
+            "slot1": {
+                "dexnumber": checkDex(ih[0x00002190]),
+                "nickname": grabName([\
+                    ih[0x000022F9], \
+                    ih[0x000022FA], \
+                    ih[0x000022FB], \
+                    ih[0x000022FC], \
+                    ih[0x000022FD], \
+                    ih[0x000022FE], \
+                    ih[0x000022FF], \
+                    ih[0x00002300], \
+                    ih[0x00002301], \
+                    ih[0x00002302]\
+                ]),
+                "ball": "",
+                "shiny": checkShiny([
+                    bHex(ih[0x000021AC]),
+                    bHex(ih[0x000021AD])
+                ]),
+                "level": ih[0x000021B6]
+            },
+            "slot2": {
+                "dexnumber": checkDex(ih[0x00002191]),
+                "nickname": grabName([\
+                    ih[0x00002304], \
+                    ih[0x00002305], \
+                    ih[0x00002306], \
+                    ih[0x00002307], \
+                    ih[0x00002308], \
+                    ih[0x00002309], \
+                    ih[0x0000230A], \
+                    ih[0x0000230B], \
+                    ih[0x0000230C], \
+                    ih[0x0000230D]\
+                ]),
+                "ball": "",
+                "shiny": checkShiny([
+                    bHex(ih[0x000021DC]),
+                    bHex(ih[0x000021DD])
+                ]),
+                "level": ih[0x000021E6]
+            },
+            "slot3": {
+                "dexnumber": checkDex(ih[0x00002192]),
+                "nickname": grabName([\
+                    ih[0x0000230F], \
+                    ih[0x00002310], \
+                    ih[0x00002311], \
+                    ih[0x00002312], \
+                    ih[0x00002313], \
+                    ih[0x00002314], \
+                    ih[0x00002315], \
+                    ih[0x00002316], \
+                    ih[0x00002317], \
+                    ih[0x00002318]\
+                ]),
+                "ball": "",
+                "shiny": checkShiny([
+                    bHex(ih[0x0000220C]),
+                    bHex(ih[0x0000220D])
+                ]),
+                "level": ih[0x00002216]
+            },
+            "slot4": {
+                "dexnumber": checkDex(ih[0x00002193]),
+                "nickname": grabName([\
+                    ih[0x0000231A], \
+                    ih[0x0000231B], \
+                    ih[0x0000231C], \
+                    ih[0x0000231D], \
+                    ih[0x0000231E], \
+                    ih[0x0000231F], \
+                    ih[0x00002320], \
+                    ih[0x00002321], \
+                    ih[0x00002322], \
+                    ih[0x00002323]\
+                ]),
+                "ball": "",
+                "shiny": checkShiny([
+                    bHex(ih[0x0000223C]),
+                    bHex(ih[0x0000223D])
+                ]),
+                "level": ih[0x00002246]
+            },
+            "slot5": {
+                "dexnumber": checkDex(ih[0x00002194]),
+                "nickname": grabName([\
+                    ih[0x00002325], \
+                    ih[0x00002326], \
+                    ih[0x00002327], \
+                    ih[0x00002328], \
+                    ih[0x00002329], \
+                    ih[0x0000232A], \
+                    ih[0x0000232B], \
+                    ih[0x0000232C], \
+                    ih[0x0000232D], \
+                    ih[0x0000232E]\
+                ]),
+                "ball": "",
+                "shiny": checkShiny([
+                    bHex(ih[0x0000226C]),
+                    bHex(ih[0x0000226D])
+                ]),
+                "level": ih[0x00002276]
+            },
+            "slot6": {
+                "dexnumber": checkDex(ih[0x00002195]),
+                "nickname": grabName([\
+                    ih[0x00002330], \
+                    ih[0x00002331], \
+                    ih[0x00002332], \
+                    ih[0x00002333], \
+                    ih[0x00002334], \
+                    ih[0x00002335], \
+                    ih[0x00002336], \
+                    ih[0x00002337], \
+                    ih[0x00002338], \
+                    ih[0x00002339]\
+                ]),
+                "ball": "",
+                "shiny": checkShiny([
+                    bHex(ih[0x0000229C]),
+                    bHex(ih[0x0000229D])
+                ]),
+                "level": ih[0x000022A6]
+            }
+        }
+
+        # Finally, writes it to the team.json file
+        file = open("../team.json","w")
+        file.write(json.dumps(pokemon))
+        file.close()
+    time.sleep(0.1)
